be62b14ab774b51a5c23a5932a034f5e39fb93fe
aravind886 <aravindcp@carestack.com>
2025-02-07 15:35:09 +0530

    geolocation updated

--- src/app/api/geo/route.ts
+++ src/app/api/geo/route.ts
@@ -1,37 +1,35 @@
-
-import { NextResponse } from 'next/server';
-import type { NextFetchEvent, NextRequest } from 'next/server';
-import { geolocation, ipAddress } from "@vercel/edge";
-import countries from "@/components/countries.json"
-import { NextURL } from 'next/dist/server/web/next-url';
+import { NextResponse } from "next/server";
+import type { NextFetchEvent, NextRequest } from "next/server";
+import { ipAddress } from "@vercel/edge";
+import { geolocation } from "@vercel/functions";
+import countries from "@/components/countries.json";
 
 export const config = {
- runtime: 'edge',
+  runtime: "edge",
 };
 
-export async function GET(
- request: NextRequest,
-) {
+export async function GET(request: NextRequest) {
   try {
-    const { nextUrl: url, geo }: any = request
-    console.log(geo)
-  if(!geo ){
-  return new Response(JSON.stringify({request}))
-  }
-  const country = geo.country || 'US'
-  const city = geo.city || 'San Francisco'
-  const region = geo.region || 'CA'
-  const ip = ipAddress(request) || "unknown";
+    const { nextUrl: url, geo }: any = request;
+    const ipa = request.headers.get("x-forwarded-for");
+    // console.log(geolocation(request),'geolocation12')
+    const geoLocation = geolocation(request);
 
-  const countryInfo:any = countries.find((x) => x.cca2 === country)
-  const currencyCode = Object.keys(countryInfo.currencies)[0]
-  const currency = countryInfo.currencies[currencyCode]
-  const languages = Object.values(countryInfo.languages).join(', ')
-  return new Response(JSON.stringify({
-    country, city, region, currencyCode, currency, languages,ip
-  }));
+    const country = geo.country || "US";
+    const city = geo.city || "San Francisco";
+    const region = geo.region || "CA";
+    const ip = ipAddress(request) || "unknown";
+
+    const countryInfo: any = countries.find((x) => x.cca2 === country);
+    const currencyCode = Object.keys(countryInfo.currencies)[0];
+    const currency = countryInfo.currencies[currencyCode];
+    const languages = Object.values(countryInfo.languages).join(", ");
+    return new Response(
+      JSON.stringify({
+        geoLocation,
+      })
+    );
   } catch (error) {
-    return new Response(JSON.stringify(error))
+    return new Response(JSON.stringify(error));
   }
-  
 }
