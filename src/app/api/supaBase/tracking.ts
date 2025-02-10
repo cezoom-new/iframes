c40ec172d8888025abefb2aa86b3199df650da1e
Princy Saji <princysaji@carestack.com>
2025-02-10 11:51:15 +0530

    track data added

--- src/app/api/supaBase/tracking.ts
+++ src/app/api/supaBase/tracking.ts
@@ -8,41 +8,32 @@ const supabaseUrl: string = process.env.SUPABASE_URL || "";
 const supabaseKey: string = process.env.SUPABASE_ANON_KEY || "";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const trackPageView = async (loc: any, eventType: string) => {
-  const url = loc.href;
-  const customerValue = loc.search.replace("?domain=", "")
-  // console.log(`Tracking ${eventType} for: ${url} ${customerValue}`);
- const [location, setLocation] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+export const trackPageView = async (
+  loc: any,
+  location: any,
+  browseDatas: any,
+  eventType: string
+) => {
+  const url = loc;
+  const customerValue = loc.search.replace("?domain=", "");
 
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useEffect(() => {
-    const fetchLocation = async () => {
-      try {
-        const response = await fetch('/');
-        const locationData = response.headers.get('x-location-data');
-        if (locationData) {
-          setLocation(JSON.parse(locationData));
-        } else {
-          setError("Location data not found");
-        }
-      } catch (err) {
-        setError('Failed to fetch location');
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchLocation();
-  }, []);
+  console.log("location",location);
 
   try {
-    console.log("location",location)
-
     const { data, error } = await supabase
-      .from('page_views')
-      .insert([{ url, event_type: eventType, customer: customerValue}]);
+      .from("page_views")
+      .insert([
+        {
+          current_path: url.href,
+          e_type: eventType,
+          utm_campaign: customerValue,
+          domain: url.origin,
+          browser: browseDatas[0].browser,
+          os: browseDatas[0].os,
+          entrypath: browseDatas[1].entryPath,
+          referrer_url: browseDatas[2].referrer
+        },
+      ]);
 
     if (error) {
       console.error("Error inserting page view:", error.message);
