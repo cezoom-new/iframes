ed4417c0fd8fc589d0f70e8683a7883439a87444
Princy Saji <princysaji@carestack.com>
2025-02-13 17:42:23 +0530

    api cookie condition checking for location

--- src/app/api/location/route.ts
+++ src/app/api/location/route.ts
@@ -1,11 +1,21 @@
+import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   if (req.method == "POST") {
-    const locationData = req.headers.get("x-location-data");
-    const locationIp = req.headers.get("x-your-ip-address");
-    return new Response(JSON.stringify({ locationData, locationIp }), {
-      status: 200,
-    });
+    const cookieStore = await cookies();
+    const location = cookieStore.get("_loc")?.value;
+    console.log(location, "mylocation");
+    if (location) {
+      return new Response(JSON.stringify(location), {
+        status: 200,
+      });
+    } else {
+      const locationData = req.headers.get("x-location-data");
+      const locationIp = req.headers.get("x-your-ip-address");
+      return new Response(JSON.stringify({ locationData, locationIp }), {
+        status: 200,
+      });
+    }
   }
 }
