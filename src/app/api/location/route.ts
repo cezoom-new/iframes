862cf6d5c28b0919d8734ad77e5e4e31f6e85596
aravind886 <aravindcp@carestack.com>
2025-02-13 23:11:04 +0530

    ip adress setting changes

--- src/app/api/location/route.ts
+++ src/app/api/location/route.ts
@@ -3,16 +3,18 @@ import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   if (req.method == "POST") {
+    const locationIp = req.headers.get("x-forwarded-for");
+    console.log(locationIp, "locationIp");
     const cookieStore = await cookies();
     const location = cookieStore.get("_loc")?.value;
-    console.log(location, "mylocation");
     if (location) {
       return new Response(JSON.stringify(location), {
         status: 200,
       });
     } else {
       const locationData = req.headers.get("x-location-data");
-      const locationIp = req.headers.get("x-your-ip-address");
+      const locationIp = req.headers.get("x-forwarded-for");
+      console.log(locationIp, "locationIp");
       return new Response(JSON.stringify({ locationData, locationIp }), {
         status: 200,
       });
