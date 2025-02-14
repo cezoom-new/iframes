e947fb58524deb05ee82a4edcd28181edff700e4
aravind886 <aravindcp@carestack.com>
2025-02-14 17:13:50 +0530

    code format

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -15,44 +15,46 @@ export async function POST(req: NextRequest) {
     type: "region",
   });
 
-  const countryName = locations?.country?  regionNamesInEnglish.of(locations?.country):'countryName not available'
+  const countryName = locations?.country
+    ? regionNamesInEnglish.of(locations?.country)
+    : "countryName not available";
   // if (countryName && locationIpAddress) {
-    try {
-      const { data, error } = await supabase
-        .from("iframe_users")
-        .insert([
-          {
-            meta: meta,
-            location: countryName,
-            ip_address: locationIpAddress,
-            browser: browserData.systemConfig.browser,
-            practice_name: customerValue,
-          },
-        ])
-        .select()
-        .single();
+  try {
+    const { data, error } = await supabase
+      .from("iframe_users")
+      .insert([
+        {
+          meta: meta,
+          location: countryName,
+          ip_address: locationIpAddress,
+          browser: browserData.systemConfig.browser,
+          practice_name: customerValue,
+        },
+      ])
+      .select()
+      .single();
 
-      /******
-       * Setting cookie for userDetails
-       * call  session Api
-       *******/
-      if (data && Object.keys(data)?.length) {
-        cookieStore.set("_UID", data?.id, {
-          sameSite: "none",
-          secure: true,
-          maxAge: 60 * 60 * 24 * 30 * 12,
-        });
-      }
-
-      if (error) {
-        console.error("Error inserting page view:", error.message);
-        return new Response("", { status: 500 });
-      } else {
-        return new Response("success", { status: 200 });
-      }
-    } catch (err) {
-      console.error("Error tracking page view:", err);
-      return new Response("", { status: 500 });
+    /******
+     * Setting cookie for userDetails
+     * call  session Api
+     *******/
+    if (data && Object.keys(data)?.length) {
+      cookieStore.set("_UID", data?.id, {
+        sameSite: "none",
+        secure: true,
+        maxAge: 60 * 60 * 24 * 30 * 12,
+      });
     }
+
+    if (error) {
+      console.error("Error inserting page view:", error.message);
+      return new Response("", { status: 500 });
+    } else {
+      return new Response("success", { status: 200 });
+    }
+  } catch (err) {
+    console.error("Error tracking page view:", err);
+    return new Response("", { status: 500 });
+  }
   // }
 }
