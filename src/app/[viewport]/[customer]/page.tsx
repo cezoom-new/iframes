55ce91a9f120649f3b06832b21a1204d29d02d44
aravind886 <aravindcp@carestack.com>
2025-02-18 19:02:08 +0530

    latest build deployed

--- src/app/[viewport]/[customer]/page.tsx
+++ src/app/[viewport]/[customer]/page.tsx
@@ -3,114 +3,22 @@ import {
   getBannerByID,
   getCampaignByID,
   getCookiesData,
+  getViewPortByProductRegion,
+  getViewPorts,
 } from "@/sanity/lib/queries";
 import { getCampaigns } from "@/utils/getCampaigns";
 import Campaign from "../../campaigns/Campaign";
 import customerDB from "../../../../database.json";
 import NotFound from "@/app/not-found";
+import { getMethod } from "@/utils/helper";
+import { fetchAllViewport, fetchViewportByDimensionValue } from "@/utils/Api";
+
+export const revalidate: number = 86400  //  60 * 60 * 24 equals to one day
+
 
 export async function generateStaticParams() {
-
-  const fetchAllViewport = async () => {
-    try {
-      const res = await fetch(`${process.env.PROJECT_URL}/api/viewports`, {
-        method: "GET",
-        headers: {
-          Authorization: `${process.env.TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      });
-      if (!res?.ok) {
-        throw new Error(`Error ${res.status}: ${res.statusText}`);
-      } else {
-        const data = await res.json();
-        return data?.data ?? null;
-      }
-    } catch (error: any) {
-      console.error({ error });
-      throw new Error(error);
-    }
-  };
-
-
-
-  const fetchCampaignByIDs = async (
-    campaignIDs: string[],
-    viewport: string,
-    customer: string
-  ) => {
-    const url = new URL(`${process.env.PROJECT_URL}/api/campaigns`);
-    try {
-      const res = await fetch(url, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `${process.env.TOKEN}`,
-        },
-        method: "POST",
-        body: JSON.stringify({ campaignIDs }),
-        next: {
-          tags: [viewport, customer],
-        },
-      });
-      if (!res?.ok) {
-        throw new Error(`Error ${res.status}: ${res.statusText}`);
-      } else {
-        return (await res.json())?.data;
-      }
-    } catch (error: any) {
-      throw new Error("ERROR", error);
-    }
-  };
-
-  const fetchBannerByID = async (
-    bannerID: string,
-    viewport: string,
-    customer: string
-  ) => {
-    try {
-      const url = new URL(`${process.env.PROJECT_URL}/api/banners`);
-      url.searchParams.append("banner-id", bannerID);
-      const res = await fetch(url, {
-        next: { tags: [viewport, customer] },
-        headers: {
-          Authorization: `${process.env.TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      });
-      if (!res.ok) {
-        throw new Error(`Error ${res.status}: ${res.statusText}`);
-      } else {
-        return (await res.json())?.data;
-      }
-    } catch (error: any) {
-      throw new Error("ERROR", error);
-    }
-  };
-
-  const fetchCookieSettings = async (viewport: string, customer: string) => {
-    const url = new URL(`${process.env.PROJECT_URL}/api/cookie-settings`);
-    try {
-      const res = await fetch(url, {
-        next: { tags: [viewport, customer] },
-
-        method: "GET",
-        headers: {
-          Authorization: `${process.env.TOKEN}`,
-          "Content-Type": "application/json",
-        },
-      });
-      if (!res.ok) {
-        throw new Error(`Error ${res.status}: ${res.statusText}`);
-      } else {
-        return (await res.json())?.data;
-      }
-    } catch (error: any) {
-      throw new Error("ERROR", error);
-    }
-  };
   // const viewports = await runQuery(getViewPorts());
-  const viewports = await fetchAllViewport();
+  const viewports = await fetchAllViewport()
   const allParams: any = [];
 
   for (const port of viewports) {
@@ -128,46 +36,14 @@ export async function generateStaticParams() {
 }
 
 export default async function ViewPort({ params }: { params: any }) {
-  const fetchViewportByDimensionValue = async (
-    viewport: string,
-    customer: string
-  ) => {
-    const url = new URL(`${process.env.PROJECT_URL}/api/viewports`);
-    url.searchParams.append("slug", viewport);
-
-    try {
-      const res: any = await fetch(url, {
-        next: { tags: [viewport, customer] },
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${process.env.TOKEN}`,
-        },
-      });
-      if (res.ok) {
-        const a = await res.json()
-        console.log(a,'wwwwwwwwwwwwwee')
-        return (a?.data);
-      } else {
-        console.error("Something went wrong");
-      }
-    } catch (error) {
-      console.log(error);
-      // throw new Error("Unexpected Error");
-    }
-  };
   const { viewport, customer } = await params;
   const viewportData = await fetchViewportByDimensionValue(viewport, customer);
-
+  
   if (!viewportData) {
     return <NotFound />;
   }
 
-  const requiredCampaigns = await getCampaigns(
-    viewport,
-    customer,
-    viewportData
-  );
+  const requiredCampaigns = await getCampaigns(viewport,customer, viewportData);
   const campaigns =
     requiredCampaigns && requiredCampaigns.length !== 0
       ? await Promise.all(
@@ -191,9 +67,7 @@ export default async function ViewPort({ params }: { params: any }) {
     ? await runQuery(getBannerByID(), { bannerID }, [bannerID])
     : null;
 
-  const cookies = viewportData.isCookieShow
-    ? await runQuery(getCookiesData())
-    : null;
+  const cookies = await runQuery(getCookiesData());
 
   // const campaign = campaigns[Math.floor(Math.random() * campaigns.length)];
   // const campaigns = campaigns.map((campaign: any) => campaign._id )
