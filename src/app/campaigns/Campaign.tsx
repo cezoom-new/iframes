b535ce7f3ab190b2c9849b60c71dd94c8988f817
aravind886 <aravindcp@carestack.com>
2025-02-20 18:20:59 +0530

    All Api validated using token

--- src/app/campaigns/Campaign.tsx
+++ src/app/campaigns/Campaign.tsx
@@ -55,7 +55,7 @@ function Campaign({
   const getUserDetails = new GetUserDevice().getTrackData();
   const fetchLocation = async () => {
     const response = await getLocationDetails();
-    const locationData = response?.region;
+    const locationData = response?.country;
     const locationIp = response.ipAddress;
     return { locationData, locationIp };
   };
