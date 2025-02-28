9a9c56213eece66b282b3fc651aea07c5b86488b
aravind886 <aravindcp@carestack.com>
2025-02-28 16:22:30 +0530

    cache settings

--- src/app/sign/link/[slug]/route.ts
+++ src/app/sign/link/[slug]/route.ts
@@ -1,14 +1,34 @@
 import { runQuery } from "@/sanity/lib/client";
+import { createClient } from "@supabase/supabase-js";
+import { redirect } from "next/dist/server/api-utils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, { params }: any) {
-  const url = req.nextUrl;
-  const campaignSlug = params.slug;
+  const url:any = req.nextUrl.searchParams;
+  const email =url.get('email')
+  const team = await params.slug;
+
+  const supabaseUrl = process.env.SUPABASE_PROJECT_URL || "";
+  const supabaseKey = process.env.SUPABASE_ANON_PUBLIC || "";
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  const metadata =  Object.fromEntries(req.headers.entries())
   try {
     const res = await fetch(
-      `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2024-11-25/data/query/${process.env.NEXT_PUBLIC_SANITY_DATASET}?query=*%5B_type+%3D%3D+%22emailSignature%22+%26%26+slug.current+%3D%3D%22${campaignSlug}%22%5D%7B%0A++%22url%22%3AemailSignatureCampaignList%5B0%5D-%3E%0A+++url%0A++%0A%7D%5B0%5D%0A++%0A%0A`
+      `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2024-11-25/data/query/${process.env.NEXT_PUBLIC_SANITY_DATASET}?query=*%5B_type+%3D%3D+%22emailSignature%22+%26%26+slug.current+%3D%3D%22${team}%22%5D%7B%0A%0A++%22url%22%3AemailSignatureCampaignList%5B0%5D-%3Eurl%2C%0A++%22campaign%22%3AemailSignatureCampaignList%5B0%5D-%3Eslug.current+%0A++%0A++%0A+++%0A++%0A%7D%5B0%5D%0A++%0A%0A`
+
     );
     const response = await res.json();
+    const { data, error } = await supabase.from("email_signature_logs").insert([
+      {
+        event_type: "Email Link Clicked",
+        sender_email: email,
+        campaign: response.result.campaign,
+        meta: metadata,
+        team: team,
+        redirect_url:response.result.url
+      },
+    ]);
+
     return NextResponse.redirect(new URL(response.result.url));
   } catch (error) {
     return new NextResponse("", {
