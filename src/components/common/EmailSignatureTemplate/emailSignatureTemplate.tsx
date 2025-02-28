7c56555f19a6e6010225859956b76a70f44d6493
Princy Saji <princysaji@carestack.com>
2025-02-28 14:57:15 +0530

    changes ui

--- src/components/common/EmailSignatureTemplate/emailSignatureTemplate.tsx
+++ src/components/common/EmailSignatureTemplate/emailSignatureTemplate.tsx
@@ -13,9 +13,6 @@ export default function EmailSignatureTemplate(props: {
   const hiddenDivRef = useRef<HTMLDivElement | null>(null);
   const searchParams = useSearchParams();
   const emailId = searchParams.get("email");
-  const fullName = searchParams.get("name");
-  const phoneNumber = searchParams.get("phone");
-  const designation = searchParams.get("role");
 
   const copyToClipboard = async () => {
     if (hiddenDivRef.current) {
@@ -40,78 +37,36 @@ export default function EmailSignatureTemplate(props: {
   };
 
   return (
-    <div className="bg-slate-300 p-6">
-      <table className="border border-gray-800 mb-4">
-        <tbody>
-          <tr>
-            <td className="p-2">Name</td>
-            <td className="p-2">{fullName}</td>
-          </tr>
-          <tr>
-            <td className="p-2">Designation</td>
-            <td className="p-2">{designation}</td>
-          </tr>
-          <tr>
-            <td className="p-2">Phone</td>
-            <td className="p-2">{phoneNumber}</td>
-          </tr>
-          <tr>
-            <td className="p-2">Email</td>
-            <td className="p-2">{emailId}</td>
-          </tr>
-          <tr>
-            <td className="p-2">Logo</td>
-            <td className="p-2">
-              <img src="https://cdn.sanity.io/images/bgk0i4de/dev/e0646e05f8a50f0eee9e377c67e8287828b65a7a-199x24.svg" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="p-6 justify-items-center h-screen flex flex-col items-center justify-center">
       {/* Hidden container to hold the HTML we want to copy */}
       <div ref={hiddenDivRef} style={{ display: "none" }}>
-        <a
-          href={
-            props.link +
-            `?email=${emailId}&name=${fullName}&phone=${phoneNumber}&role=${designation}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href={props.link + `?email=${emailId}`} target="_blank" rel="noopener noreferrer">
           <img
-            src={
-              props.redirectUrl +
-              `?email=${emailId}&name=${fullName}&phone=${phoneNumber}&role=${designation}`
-            }
+            src={props.redirectUrl + `?email=${emailId}`}
             alt="Email Signature Image"
             style={{ border: "none" }}
           />
         </a>
       </div>
-      <div>
-        <button className="bg-green-600 p-2 rounded-md" onClick={copyToClipboard}>
+
+      {/* Preview of the email signature */}
+      <div className="w-2/3">
+        <a href={props.link + `?email=${emailId}`} target="_blank" rel="noopener noreferrer">
+          <img
+            src={props.redirectUrl + `?email=${emailId}`}
+            alt="Email Signature Image"
+            style={{ border: "none" }}
+          />
+        </a>
+      </div>
+      <div className="flex pt-6 gap-3  flex-col relative">
+        <button
+          className="bg-green-600 p-2 rounded-md text-white"
+          onClick={copyToClipboard}
+        >
           Copy Signature
         </button>
-        <span className="pl-4">{copySuccess}</span>
-      </div>
-      {/* Preview of the email signature */}
-      <div className="w-1/2 h-1/2">
-        <a
-          href={
-            props.link +
-            `?email=${emailId}&name=${fullName}&phone=${phoneNumber}&role=${designation}`
-          }
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={
-              props.redirectUrl +
-              `?email=${emailId}&name=${fullName}&phone=${phoneNumber}&role=${designation}`
-            }
-            alt="Email Signature Image"
-            style={{ border: "none" }}
-          />
-        </a>
+        <div className="flex w-full absolute -bottom-8 text-center justify-center">{copySuccess}</div>
       </div>
     </div>
   );
