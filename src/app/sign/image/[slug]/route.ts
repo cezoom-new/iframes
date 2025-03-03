7b2b6fd68a5ed8309b3547b267c06c51486bb154
aravind886 <aravindcp@carestack.com>
2025-03-03 13:54:25 +0530

    experimental change for cache

--- src/app/sign/image/[slug]/route.ts
+++ src/app/sign/image/[slug]/route.ts
@@ -36,7 +36,7 @@ export async function GET(req: NextRequest, { params }: any) {
       headers: {
         "Content-Type": "image/jpeg",
         "Cache-Control":
-          "public, max-age=3600, s-maxage=3600, stale-while-revalidate", 
+        "public, max-age=240, s-maxage=240, stale-while-revalidate",
       },
     });
   } catch (error) {
