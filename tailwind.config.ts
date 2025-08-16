6ad2cc1ee372d5f6c6ec52f5b5032f2069f3fa7c
Princy Saji <princysaji@carestack.com>
2025-08-16 10:05:57 +0000

    Merged in dev (pull request #126)
    Dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/iframes-marketing into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/iframes-marketing into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/iframes-marketing into dev
    
    * Merged in newLayout (pull request #111)
    
    NewLayout
    
    * new layout changes
    
    * new layout changes
    
    * login page  new layout updates
    
    * Merged in newLayout (pull request #113)
    
    NewLayout
    
    * new layout changes
    
    * new layout changes
    
    * login page  new layout updates
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/iframes-marketing into newLayout
    
    * padding bottom for image was fixed
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/iframes-marketing into dev
    
    * max width added
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/iframes-marketing into dev
    
    * new layout changes
    
    * Merged in fallbackLogic (pull request #117)
    
    FallbackLogic
    
    * Merged in dev (pull request #115)
    
    Dev
    
    * Merged in cs-learn-tweaks (pull request #88)
    
    Cs learn tweaks
    
    * Merged in dev (pull request #65)
    
    gap set for md
    
    * Merged in dev (pull request #66)
    
    Dev
    
    * Merged in dev (pull request #67)
    
    iframe width issue fixed
    
    * Merged in dev (pull request #68)
    
    color added
    
    * Merged in dev (pull request #69)
    
    fixes of alignment of image and content, video dsplay hidden on mobile device
    
    * Merged in dev (pull request #70)
    
    video added to left side
    
    * Merged in dev (pull request #71)
    
    promo code style fix
    
    * Merged in dev (pull request #72)
    
    gap addedbetween promo code
    
    * Merged in dev (pull request #73)
    
    Dev
    
    * Merged in dev (pull request #74)
    
    bg image place to the center
    
    * Merged in dev (pull request #75)
    
    min height set to the layout
    
    * Merged in dev (pull request #76)
    
    banner gap added
    
    * Merged in dev (pull request #77)
    
    Dev
    
    * Merged in dev (pull request #78)
    
    Dev
    
    * Merged in dev (pull request #79)
    
    email si…
    * email signature links separated
    
    * template changes
    
    * style fixes
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/iframes-marketing into dev
    
    * space added
    
    * changed added
    
    * empty issue fixed
    
    * Alignment issues fixes
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/iframes-marketing into dev
    
    * layout gap changes
    
    * email signature style changes
    
    * Merge branch 'dev' of https://bitbucket.org/cstack-bb/iframes-marketing into dev
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/iframes-marketing into dev
    
    * build error fix
    
    * Merge branch 'main' of https://bitbucket.org/cstack-bb/iframes-marketing into dev

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,16 +61,41 @@ export default {
   				'Manrope',
   				'sans-serif'
   			],
-			geist: ['Geist', 'sans-serif']
+  			geist: [
+  				'Geist',
+  				'sans-serif'
+  			]
   		},
   		screens: {
   			xs: '450px',
-			xmd: '936px'
+  			xmd: '936px'
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
